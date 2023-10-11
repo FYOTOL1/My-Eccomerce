dda68407ef983d9e3a18df7a37e57259fc89030b
EGY-Temble <fyotol@gmail.com>
2023-10-12 00:28:23 +0300

    sa

--- app/src/components/home/Services/Featured.jsx
+++ app/src/components/home/Services/Featured.jsx
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "../../../style/css/home/aboutServices.css";
 
 export default function Featured() {
   const Store = useSelector((state) => state?.featured);
+  const [data, setdata] = useState([]);
+  const maping = () => {
+    return (
+      Store &&
+      Store?.data?.map((e) => (
+        <div key={e?._id} className="card">
+          <div className="imgParent">
+            <img className="img" src={e?.img} alt={e?.name} />
+          </div>
+          <h3>{e?.name}</h3>
+          <div className="desc">
+            <p>{e?.info}</p>
+          </div>
+          <div className="info">
+            <div className="rate">
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star-half-stroke"></i>
+            </div>
+            <p className="price">
+              <span>$</span>
+              {e?.price}
+            </p>
+          </div>
+        </div>
+      ))
+    );
+  };
+
+  useEffect(() => {
+    setdata(maping());
+  }, []);
   return (
     <div className="featured">
       <h2>featured services</h2>
-      <div className="cards">
-        {Store &&
-          Store?.data?.map((e) => (
-            <div key={e?._id} className="card">
-              <div className="imgParent">
-                <img className="img" src={e?.img} alt={e?.name} />
-              </div>
-              <h3>{e?.name}</h3>
-              <div className="desc">
-                <p>{e?.info}</p>
-              </div>
-              <div className="info">
-                <div className="rate">
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star"></i>
-                  <i className="fa-solid fa-star-half-stroke"></i>
-                </div>
-                <p className="price">
-                  <span>$</span>
-                  {e?.price}
-                </p>
-              </div>
-            </div>
-          ))}
-      </div>
+      <div className="cards">{data && data}</div>
     </div>
   );
 }
