82a863cdac18be130ec5d86a63e72927cf89e0f6
EGY-Temble <fyotol@gmail.com>
2023-10-12 14:54:41 +0300

    fa

--- app/src/components/home/Services/Featured.jsx
+++ app/src/components/home/Services/Featured.jsx
@@ -4,18 +4,18 @@ import "../../../style/css/home/aboutServices.css";
 import { GetFeaturedServices } from "../../../Redux/slices/featuredServicesSlice";
 
 export default function Featured() {
-  const Store = useSelector((state) => state?.featured);
-  const dispatch = useDispatch();
+  // const Store = useSelector((state) => state?.featured);
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(GetFeaturedServices());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(GetFeaturedServices());
+  // }, []);
 
   return (
     <div className="featured">
       <h2>featured services</h2>
       <div className="cards">
-        {Store.data &&
+        {/* {Store.data &&
           Store?.data?.map((e) => (
             <div key={e?._id} className="card">
               <div className="imgParent">
@@ -39,7 +39,7 @@ export default function Featured() {
                 </p>
               </div>
             </div>
-          ))}
+          ))} */}
       </div>
     </div>
   );
