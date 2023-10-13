ef07d085db1ada89b64fadf623929d402b25a499
EGY-Temble <fyotol@gmail.com>
2023-10-13 09:34:08 +0300

    add coers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const featuredApi = require("./apis/featuredApi");
 const popularApi = require("./apis/popularApi");
+const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://3ayez.vercel.app/",
+  })
+);
 
 app.use(featuredApi);
 app.use(popularApi);
