7e5b0492fd6f02f730acc6ef6c26c1af5a441a83
EGY-Temble <fyotol@gmail.com>
2023-10-24 20:29:31 +0300

    Add Upload Action And Add Delete All And Add Products

--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = "https://backend-fn1s.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
